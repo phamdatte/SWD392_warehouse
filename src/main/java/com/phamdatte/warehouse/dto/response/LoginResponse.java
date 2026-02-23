3ef057e3d53112e5c7e1bcc3c329ad210c790260
Pham Dat <pdatte1999@gmail.com>
2026-02-24 00:13:42 +0700

    fix: @Builder.Default for tokenType default value in LoginResponse

--- src/main/java/com/phamdatte/warehouse/dto/response/LoginResponse.java
+++ src/main/java/com/phamdatte/warehouse/dto/response/LoginResponse.java
@@ -5,6 +5,7 @@ import lombok.*;
 @Getter @Setter @Builder @AllArgsConstructor @NoArgsConstructor
 public class LoginResponse {
     private String token;
+    @Builder.Default
     private String tokenType = "Bearer";
     private Integer userId;
     private String username;
